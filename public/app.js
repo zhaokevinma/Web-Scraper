601e3c9fcc0e74c0f2b33b8ec28cf4b9cbfd2941
Kevin Malone <zhaokevinma@gmail.com>
2019-10-31 17:59:16 -0400

    save -> saved + disable true completed - post request needed

--- public/app.js
+++ public/app.js
@@ -4,8 +4,38 @@ $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    $("#articles").append("<button id='" + data[i]._id + "'> Save " + "</button>");
+    $("#articles").append("<button class='savearticle' id='" + data[i]._id + "'> Save " + "</button>");
   }
+});
+
+// When you click the save button to save an article
+$(document).on("click", ".savearticle", function(event) {
+  event.preventDefault();
+  // // Grab the id associated with the article from the submit button
+  // var thisId = $(this).attr("id");
+
+  // // Run a POST request to change the note, using what's entered in the inputs
+  // $.ajax({
+  //   method: "POST",
+  //   url: "/articles/" + thisId,
+  //   data: {
+  //     // Value taken from title input
+  //     title: $("#titleinput").val(),
+  //     // Value taken from note textarea
+  //     body: $("#bodyinput").val()
+  //   }
+  // })
+  //   // With that done
+  //   .then(function(data) {
+  //     // Log the response
+  //     console.log(data);
+  //     // Empty the notes section
+  //     $("#notes").empty();
+  //   });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $(this).text("Saved");
+  $(this).attr("disabled", true);
 });
 
 
