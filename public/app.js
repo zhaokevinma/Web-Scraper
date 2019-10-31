10bbf2ac59f501089356a8942a40726aa3e119d7
Kevin Malone <zhaokevinma@gmail.com>
2019-10-31 17:40:43 -0400

    save button added for each artile - post request needed to change save in database

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<button id='" + data[i]._id + "'> Save " + "</button>");
   }
 });
 
