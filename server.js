55c134724b67b537123ddb7e10a7cdae5a1a5f51
Kevin Malone <zhaokevinma@gmail.com>
2019-11-03 14:45:55 -0500

    heroku

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/webscraper";
+var MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 
